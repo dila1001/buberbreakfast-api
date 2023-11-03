9c1c6c8605a3d1dc32dd1e771fed6379a97684b6
Adila Razmi <adilarazmi@Adilas-MBP.localdomain>
2023-11-03 22:56:29 +0100

    refactor: refactor controller & services

--- BuberBreakfast.Api/ServiceErrors/Errors.Breakfast.cs
+++ BuberBreakfast.Api/ServiceErrors/Errors.Breakfast.cs
@@ -6,6 +6,16 @@ public static class Errors
 {
     public static class Breakfast
     {
+        public static Error InvalidName =>
+            Error.Validation(
+                code: "Breakfast.InValidName",
+                description: $"Breakfast name should be between {Models.Breakfast.MinNameLength}-{Models.Breakfast.MaxNameLength} charaters long."
+            );
+        public static Error InvalidDescription =>
+            Error.Validation(
+                code: "Breakfast.InValidDescription",
+                description: $"Description should be between {Models.Breakfast.MinDescriptionLength}-{Models.Breakfast.MaxDescriptionLength} charaters long."
+            );
         public static Error NotFound =>
             Error.NotFound(code: "Breakfast.NotFound", description: "Breakfast not found");
     }
